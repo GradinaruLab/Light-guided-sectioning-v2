4a97a4f87ba5815325c079070a060c1a053c9472
anatkahan <anatk@caltech.edu>
2021-09-26 18:44:00 -0700

    Update find_stained_cells.m

--- find_stained_cells.m
+++ find_stained_cells.m
@@ -1,16 +1,12 @@
 function find_stained_cells
+% Anat Kahan, Cell Reports 2021
 % define the positive stained cells for LiGS
 
 
 % read the contours, after running 'get_data_suite2P'
-%cd ('C:\Users\anatk\Documents\Light_sectioning\Str39_LGS\all_tiff_to_align_combined\sess1')
 cd ('C:\Users\anatk\Documents\Light_sectioning\WT36R_LGS\tiff_Zstack_to_align\sess485')
 
 C=imread('contours.tif');
-
-% % 
-% M=imread('870um_ARC_crop.tif');
-% C=imread('870um_contours_crop.tif');
 
 % read the stained image, after using
 % 'apply_transformation_on_stained_image'
